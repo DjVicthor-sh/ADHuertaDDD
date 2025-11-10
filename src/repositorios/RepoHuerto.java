24954038894c13a781391cf70fa1d794ab316781
Victhor <victor.perezcano01@proton.me>
2025-11-10 17:54:40 +0100

    implementacion en RepoHuerto y RepoPersona de IRepositorioExtend, creacion de dominio huerto y Persona

---
+++ src/repositorios/RepoHuerto.java
@@ -0,0 +1,46 @@
+package repositorios;
+
+import java.util.List;
+import java.util.Optional;
+
+public class RepoHuerto implements IRepositorioExtend {
+    @Override
+    public Optional findByIdOptional(Object o) {
+        return Optional.empty();
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Object o) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public boolean existsById(Object o) {
+        return false;
+    }
+
+    @Override
+    public Object findById(Object o) {
+        return null;
+    }
+
+    @Override
+    public List findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Object save(Object entity) {
+        return null;
+    }
+}
